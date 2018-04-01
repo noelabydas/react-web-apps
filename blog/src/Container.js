5ed5b97e9da3dad67d9e3ecc219622800579bc74
leonatwork <noelabydas@gmail.com>
2018-04-01 22:51:19 +0530

    created blog app

---
+++ blog/src/Container.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PostPreview from './Post-preview';
+
+class Container extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            <PostPreview />
+            <hr/>
+            <PostPreview />
+            <hr/>
+            <PostPreview />
+            <hr/>
+            <PostPreview />
+            <hr/>
+            <div className="clearfix">
+              <a className="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Container;
